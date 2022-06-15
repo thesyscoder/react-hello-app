af8488bb068ec6aeb86ed1f9f528e25f1f55295f
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-06-15 19:13:23 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Header from "./components/header"
 const App = () => {
     return (
         <div>
+            <Header/>
             <h1>Hello App</h1>
         </div>
     )
