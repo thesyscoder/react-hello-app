6638c241a1a5163624ea3c42c892ff324c4bdac6
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-06-16 05:47:26 +0530

    states

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,43 @@
+import React, { Component } from "react";
 import "../styles/styles.css";
 
-const inputChange = (event) => {
-  console.log("I was changed");
-  console.log(event.target.value);
-};
-const Header = () => {
-  return (
-    <header>
-      <h1 onClick={() => console.log("This is logo")}>Logo</h1>
-      <input onChange={inputChange} />
-    </header>
-  );
-};
+class Header extends Component {
+  state = {
+    title: "This is input data",
+    keywords: "",
+    count: 0,
+  };
+  inputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ keywords: event.target.value });
+  };
+  increment = () => {
+    this.setState((state, props) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <div
+            className="logo"
+            onClick={() => {
+              console.log("Logo clicked");
+            }}
+          >
+            Logo
+          </div>
+          <input onChange={this.inputChange}></input>
+          <div>{this.state.title}</div>
+          <div>{this.state.keywords}</div>
+          <div>{this.state.count}</div>
+          <button onClick={this.increment}>Increment</button>
+        </header>
+      </>
+    );
+  }
+}
+
 export default Header;
